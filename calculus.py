b02c7ef56b0affee62b621399aaa43adbf973723
John Wilson <j.wilson@york.ac.uk>
2016-08-16 14:18:11 +0100

    Add ability to differentiate one or more traces

--- calculus.py
+++ calculus.py
@@ -7,6 +7,8 @@ from pyxpad_utils import XPadDataItem
 
 from numpy import zeros, cumsum
 
+import numpy
+
 
 def integrate(item):
     """
@@ -46,5 +48,50 @@ def integrate(item):
     result.dim = item.dim
     result.order = item.order
     result.time = item.time
+
+    return result
+
+
+def differentiate (item):
+    """
+    Differentiates the given trace
+
+    Inputs
+    ------
+
+    item  - an XPadDataItem object (or equivalent)
+
+    Returns
+    -------
+
+    an XPadDataItem object
+
+    """
+
+    if len(item.dim) != 1:
+        raise ValueError("Can only differentiate 1D traces")
+
+    # Create a result
+    result = XPadDataItem()
+    if item.name != "":
+        result.name = "Diff(" + item.name + ")"
+    result.source = item.source
+    if item.label != "":
+        result.label = "Diff(" + item.label + ")"
+    if item.units != "":
+        result.units = item.units + item.dim[0].units + chr(0x207B) + chr(0x00B9)
+    result.dim = item.dim
+    result.order = item.order
+    result.time = item.time
+
+    time = item.dim[item.order].data
+
+    result.data = numpy.zeros(len(item.data))
+
+    for i in range(1, len(result.data)-1) :
+        result.data[i] = (item.data[i+1]-item.data[i-1])/(time[i+1]-time[i-1])
+ 
+    result.data[-1] = result.data[-2]
+    result.data[0] = result.data[1]
 
     return result
